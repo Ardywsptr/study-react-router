d63d4df809754f51ad46920a49f7ece4f699608d
Ardy wirasaputra <ardywsptr1@gmail.com>
2023-10-27 17:03:28 +0700

    Pathless Routes
    Ketika ada error pada route child maka hanya tampilan itu saja yang menampilkan error, bukan secara global

--- src/main.jsx
+++ src/main.jsx
@@ -10,40 +10,45 @@ import { action as destroyAction } from "./routes/destroy";
 import Index from './routes'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        index: true,
-        element: <Index />
-      },
-      {
-        path: 'contacts/:contactId',
-        element: <Contact />,
-        loader: contactLoader,
-        action: contactAction
-      },
-      {
-        path: 'contacts/:contactId/edit',
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: 'contacts/:contactId/destroy',
-        errorElement: <div>Oops! There was an error.</div>,
-        action: destroyAction,
-      }
-    ]
-  },
+   {
+      path: '/',
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      loader: rootLoader,
+      action: rootAction,
+      children: [
+         {
+            errorElement: <ErrorPage />,
+            children: [
+               {
+                  index: true,
+                  element: <Index />
+               },
+               {
+                  path: 'contacts/:contactId',
+                  element: <Contact />,
+                  loader: contactLoader,
+                  action: contactAction
+               },
+               {
+                  path: 'contacts/:contactId/edit',
+                  element: <EditContact />,
+                  loader: contactLoader,
+                  action: editAction,
+               },
+               {
+                  path: 'contacts/:contactId/destroy',
+                  errorElement: <div>Oops! There was an error.</div>,
+                  action: destroyAction,
+               }
+            ]
+         }
+      ]
+   },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+   <React.StrictMode>
+      <RouterProvider router={router} />
+   </React.StrictMode>,
 )
