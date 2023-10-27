83a39ca461a75ddb9ce9926ed29951847e2851af
Ardy wirasaputra <ardywsptr1@gmail.com>
2023-10-27 16:28:58 +0700

    Mutations Without Navigation
    Membuat fitur favorite

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import { Form, useFetcher, useLoaderData } from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
    const contact = await getContact(params.contactId);
    return { contact };
+}
+
+export async function action({ request, params }) {
+   let formData = await request.formData();
+
+   return updateContact(params.contactId, {
+      favorite: formData.get('favorite') == 'true'
+   })
 }
 
 export default function Contact() {
@@ -60,13 +68,14 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
-   // yes, this is a `let` for later
+   const fetcher = useFetcher();
    let favorite = contact.favorite;
+
    return (
-      <Form method="post">
+      <fetcher.Form method="post">
          <button name="favorite" value={favorite ? "false" : "true"} aria-label={favorite ? "Remove from favorites" : "Add to favorites"}>
             {favorite ? "★" : "☆"}
          </button>
-      </Form>
+      </fetcher.Form>
    );
 }
