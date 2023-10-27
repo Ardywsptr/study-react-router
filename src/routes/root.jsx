f9007d3ada310776e0d41284d9423c57958868af
Ardy wirasaputra <ardywsptr1@gmail.com>
2023-10-27 12:34:01 +0700

    URL Search Params And Get Submission
    Membuat fitur pencarian dengan mengirimkan setiap query ke param menggunakan method default formulir yaitu GET

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,8 +3,10 @@
 import { Form, Link, NavLink, Outlet, redirect, useLoaderData, useNavigation } from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
 
-export async function loader() {
-   const contacts = await getContacts();
+export async function loader({ request }) {
+   const url = new URL(request.url);
+   const q = url.searchParams.get('q')
+   const contacts = await getContacts(q);
    return { contacts };
 }
 
@@ -22,11 +24,11 @@ export default function Root() {
          <div id="sidebar">
             <h1>React Router Contacts</h1>
             <div>
-               <form id="search-form" role="search">
+               <Form id="search-form" role="search">
                   <input id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q" />
                   <div id="search-spinner" aria-hidden hidden={true} />
                   <div className="sr-only" aria-live="polite"></div>
-               </form>
+               </Form>
                <Form method="post">
                   <button type="submit">New</button>
                </Form>
